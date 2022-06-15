c326683c93b964f54ea0561f563b31be3f7ed3fc
RafaelAF <99427100+RafaelAF0000@users.noreply.github.com>
2022-06-15 08:35:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const formCadastro = require("./routers/formCadastro.js")
 formCadastro(app)
 const home = require("./routers/home.js")
 home(app)
+const login = require("./routers/login.js")
+login(app)
+
 ////////////////////////////////
 app.listen("8000", () => {
     console.log("Servidor up")
