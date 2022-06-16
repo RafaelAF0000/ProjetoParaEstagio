ba68b9a1e4eb8da7a018efabd781ea54d34a05ac
RafaelAF0000 <rafaraujofrota@gmail.com>
2022-06-16 15:15:11 -0300

    Função Mostrar Usuários Adicionada

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -25,3 +25,55 @@ fillEstadoSelect()
     }
 
 openUserDialog()
+
+    function fillEstadoTable(){
+        const tdEstado = document.getElementsByClassName("tdEstado")
+        const tdCep = document.getElementsByClassName("tdCep")
+        for (let i = 0; i < tdCep.length; i++) {
+            const cleanValue = tdCep[i].innerText.replace(/\D/g, "")
+            const url = `https://viacep.com.br/ws/${cleanValue}/json/`
+            fetch(url)
+            .then(res => {
+                if(res.ok) {
+                    return res.json()
+                }
+            })
+            .then(dadosCep => {
+                tdEstado[i].innerText = dadosCep.uf
+            })
+        }
+    }
+
+fillEstadoTable()
+
+    function filtrarEstado(){
+        const selectEstado = document.getElementById("selectEstado")
+        const trDadosUser = document.getElementsByClassName("trDadosUser")
+        const tdEstado = document.getElementsByClassName("tdEstado")
+        
+        let hasResults = false
+
+        for(let i = 0; i < trDadosUser.length; i ++) {
+
+            if (selectEstado.value != "all" && tdEstado[i].innerText != selectEstado.value) {
+                trDadosUser[i].style.display = "none"
+            }
+            else {
+                hasResults = true
+                trDadosUser[i].style.display = "table-row" //display padrão do Tr
+            }
+        }
+        
+        const tabelaUsuarios = document.getElementById("tabelaUsuarios")
+        const errorMessage = document.getElementById("errorMessage")
+        
+        if(!hasResults) {
+            tabelaUsuarios.style.display = "none"
+            errorMessage.innerText = "Sem resultados"
+        }
+        else {
+            tabelaUsuarios.style.display = "table"
+            errorMessage.innerText = ""
+        }
+
+    }
